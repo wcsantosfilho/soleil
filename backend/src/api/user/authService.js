f523cb5de10167a15e57e674f442b097b5a29b3a
Walter Santos Filho <wcsantosfilho@gmail.com>
2018-09-01 20:09:07 -0300

    Correções autenticação

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -4,27 +4,33 @@ const bcrypt = require('bcrypt')
 const User = require('./user')
 const env = require('../../.env')
 
+/* expressão regular do formato do e-mail */
 const emailRegex = /\S+@\S+\.\S+/
+/* expressão regular da senha: 6 a 20 caracteres, uma minúscula, uma maiúscula e um caractere especial */
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%;]).{6,20})/
 
+/* Rotina de resposta de erros */
 const sendErrorsFromDB = (res, dbErrors) => {
     const errors = []
     _.forIn(dbErrors.errors, error => errors.push(error.message))
     return res.status(400).json({errors})
 }
 
+/* Faz o login */
 const login = (req, res, next) => {
+    /* A requisição traz email e password */
     const email = req.body.email || ''
     const password = req.body.password || ''
 
+    /* Busca o usuário pelo email */
     User.findOne({email}, (err, user) => {
         if(err) {
             return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign(user, env.authSecret, {
-                expiresIn: "1 day"
-            })
+            const token = jwt.sign({ uid: user }, env.authSecret, { expiresIn: "1 day" })
+            /* Destructuring do objeto user em nome e email */
             const { name, email } = user
+            /* A resposta do login é um JSON (nome, email e token) */
             res.json({ name, email, token })
         } else {
             return res.status(400).send({errors: ['Usuário/Senha inválidos']})
@@ -32,6 +38,7 @@ const login = (req, res, next) => {
     })
 }
 
+/* Verifica se o token é válido */
 const validateToken = (req, res, next) => {
     const token = req.body.token || ''
 
@@ -40,13 +47,15 @@ const validateToken = (req, res, next) => {
     })
 }
 
+/* Retorna as credenciais do usuário logado */
 const userCredentials = (req, res, next) => {
     const token = req.body.token || ''
     jwt.verify(token, env.authSecret, function(err, decoded) {
         if (err) { 
             return res.status(500).send({errors: ['Falha na autenticação']})
         } else {
-            User.findById(decoded._doc._id, { password: 0 }, (err, user) => {
+            /* "password: 0" indica para o Mongoose que a password não vai retornar */
+            User.findById(decoded.uid._id, { password: 0 }, (err, user) => {
                 if(err) {
                     return sendErrorsFromDB(res, err)
                 } else {
@@ -57,6 +66,7 @@ const userCredentials = (req, res, next) => {
     })
 }
 
+/* Cria novo usuário na aplicação */
 const signup = (req, res, next) => {
     const name = req.body.name || ''
     const email = req.body.email || ''
